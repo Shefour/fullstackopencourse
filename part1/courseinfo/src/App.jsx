a07d34fe9ecdb22b5d8b9dc1ff875dc520ebefbc
Shefour <main.shefour@protonmail.com>
2023-10-18 08:47:58 +0200

    Progress

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,51 +1,53 @@
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
-  const Header = (courses) => {
+  const Header = (props) => {
+    console.log(props)
     return (
-      <div>
-        {courses.course}
-      </div>
+      <h1>
+        {props.course}
+      </h1>
     )
   }
 
-  const Content = () => {
-      return (
-        <div>
-        <p>
-          {part1} {exercises1}
-        </p>
-        <p>
-          {part2} {exercises2}
-        </p>
-        <p>
-          {part3} {exercises3}
-        </p>
-        </div>  
-    )
-  }
-  
-  const Total = () => {
+  const Content = (parts) => {
     return (
-      <div>
-        <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
-      </div>
+      parts.forEach(element => {
+        element.exercises
+      })
     )
-  }
+}
+
+const Total = (parts) => {
+  return (
+    <div>
+      <p>Number of exercises {part1.exercises + part2.exercises + part3.exercises}</p>
+    </div>
+  )
+}
 
   return (
     <div>
       <Header course={course}/>
-      <Content/>
-      <Total/>
+      <Content parts={parts}/>
+      <Total parts={parts}/>
     </div>
   )
+
 }
 
 export default App
--- sandbox/src/App.jsx
+++ sandbox/src/App.jsx
@@ -1,23 +1,14 @@
-const Hello = (props) => {
-  console.log(props)
+const App = ({ notes }) => {
   return (
     <div>
-      <p>Hello {props.name}, you're {props.age} years old</p>
-    </div>
-  )
-}
-
-const App = () => {
-  const peepos = [
-    { name: 'Peter', age: 4 },
-    { name: 'Maya', age: 10 },
-  ]
-
-  return (
-    <div>
-      <h1>Greetings</h1>
-      <p>{peepos[0].name}, {peepos[0].age}</p>
-      <p>{peepos[1].name}, {peepos[1].age}</p>
+      <h1>Notes</h1>
+      <ul>
+        {notes.map(note => 
+          <li key={note.id}>
+            {note.content}
+          </li>
+        )}
+      </ul>
     </div>
   )
 }
