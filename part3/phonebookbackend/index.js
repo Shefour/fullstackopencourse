1cf025b0c06643fab8680e92803c17cbf3d9f673
Shefour <main.shefour@protonmail.com>
2024-02-01 14:09:52 +0100

    renamed folders and so on

--- part3/phonebookbackend/index.js
+++ part3/phonebookbackend/index.js
@@ -1,7 +1,19 @@
 const express = require('express')
 const app = express()
 
-app.use(express.json())
+const requestLogger = (req, res, next) => {
+  console.log('Method:', req.method)
+  console.log('Path:  ', req.path)
+  console.log('Body:  ', req.body)
+  console.log('---')
+  next()
+}
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(express.json(), requestLogger, unknownEndpoint)
 
 let persons = [
     { 
