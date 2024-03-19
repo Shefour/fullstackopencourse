301fdb0db2cb59def2cea4c3d449fce642c3b861
Shefour <main.shefour@protonmail.com>
2024-03-19 15:17:07 +0100

    Commit Message

--- part2/phonebook/src/services/dbservice.js
+++ part2/phonebook/src/services/dbservice.js
@@ -1,10 +1,9 @@
 import axios from "axios";
 
-const baseurl = "http://localhost:3001";
+const baseurl = "/api/persons";
 
 const create_person = async (newPerson) => {
-  try {
-    const existingPerson = await axios.get(`${baseurl}/persons?name=${newPerson.name}`);
+    const existingPerson = await axios.get(`${baseurl}?name=${newPerson.name}`);
 
     if (existingPerson.data.length > 0) {
       const shouldUpdate = window.confirm(
@@ -15,7 +14,7 @@ const create_person = async (newPerson) => {
         const personToUpdate = existingPerson.data[0];
         const updatedPerson = { ...personToUpdate, number: newPerson.number };
 
-        await axios.put(`${baseurl}/persons/${personToUpdate.id}`, updatedPerson);
+        await axios.put(`${baseurl}/${personToUpdate.id}`, updatedPerson);
         return updatedPerson;
       } else {
         // User chose not to update, return the existing person
@@ -23,16 +22,13 @@ const create_person = async (newPerson) => {
       }
     } else {
       // Person doesn't exist, create a new one
-      const response = await axios.post(`${baseurl}/persons`, newPerson);
+      const response = await axios.post(`${baseurl}`, newPerson);
       return response.data;
     }
-  } catch (error) {
-    throw error;
-  }
-};
+  };
 
 const delete_person = (id) => {
-  return axios.delete(`${baseurl}/persons/${id}`);
+  return axios.delete(`${baseurl}/${id}`);
 };
 
 export default { create_person, delete_person };
